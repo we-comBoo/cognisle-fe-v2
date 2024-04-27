5ec51dc6b592ec5e5511f9261049b46673497aa5
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-04-27 21:55:25 +0900

    feat: witAuth 훅 데이터 없는 경우 추가

--- src/hooks/withAuth.tsx
+++ src/hooks/withAuth.tsx
@@ -1,16 +1,22 @@
 import { ComponentType } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import { stat } from 'fs'
 type Props = Record<string, never>
 
 const withAuth = (WrappedComponent: ComponentType<Props>) => {
   return function AuthenticatedComponent(props: Props) {
     const { data, status } = useSession()
     const router = useRouter()
-    if (status != 'loading' && data === null) {
+
+    if (status === 'loading') {
+      return null
+    }
+    if (data === null) {
       router.replace('/auth/login')
       return null
     }
+
     return <WrappedComponent {...(props as any)} />
   }
 }
