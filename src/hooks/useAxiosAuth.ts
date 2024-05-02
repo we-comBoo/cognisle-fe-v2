3eebf545ed18ac03a65e5bc42126170d7fe9bd11
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-05-02 14:14:40 +0900

    feat: remove cached file to remove zip file

--- src/hooks/useAxiosAuth.ts
+++ src/hooks/useAxiosAuth.ts
@@ -4,7 +4,6 @@ import { useEffect } from 'react'
 
 const useAxiosAuth = () => {
   const { data: session } = useSession()
-  console.log(session)
 
   useEffect(() => {
     const requestIntercept = axiosAuth.interceptors.request.use((config) => {
