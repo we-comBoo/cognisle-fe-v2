dcf9cfe1e55260d72af6bfa89b4104c227381381
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-05-07 02:47:18 +0900

    feat: Possiton 관련 스타일 코드 및 드래그 이벤트 처리 함수 props 분리

--- src/pages/drag/index.tsx
+++ src/pages/drag/index.tsx
@@ -1,61 +1,72 @@
+import styled from '@emotion/styled'
 import React, { useState } from 'react'
+import Image from 'next/image'
+interface DragPosition {
+  x: number
+  y: number
+}
 
-export default function DragExample() {
-  const [{ x, y }, setPosition] = useState({
+const mouseEventHander = (
+  e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  setPosition: React.Dispatch<React.SetStateAction<DragPosition>>,
+  x: DragPosition['x'],
+  y: DragPosition['y'],
+) => {
+  const initX = e.screenX
+  const initY = e.screenY
+
+  const mouseMoveHandler = (e: MouseEvent) => {
+    console.log('mouseMovie')
+    setPosition({
+      x: x + e.screenX - initX,
+      y: y + e.screenY - initY,
+    })
+  }
+  const mouseDownHandler = (e: MouseEvent) => {
+    console.log('mouseDown')
+    // document.removeEventListener('mousemove', mouseMoveHandler)
+  }
+  const mouseUpHandler = (e: MouseEvent) => {
+    console.log('mouseUp')
+    document.removeEventListener('mousemove', mouseMoveHandler)
+  }
+
+  document.addEventListener('mousemove', mouseMoveHandler)
+  // 한번만 실행되면 되기에 { once: true } 옵션을 추가
+  document.addEventListener('mouseup', mouseUpHandler, { once: true })
+  document.addEventListener('mousedown', mouseDownHandler, {
+    once: true,
+  })
+}
+
+export default function Drag() {
+  const [{ x, y }, setPosition] = useState<DragPosition>({
     x: 0,
     y: 0,
   })
 
   return (
-    <div className="p-4">
-      <div className="mb-2">
-        <h1 className="text-3xl font-bold">Drag</h1>
-        <span>without boundary bounce</span>
-        <span className="ml-4">
-          x:{x} y:{y}
-        </span>
-      </div>
-
-      <div className="grid items-center justify-center">
-        <div
-          className="h-24 w-24"
-          // style={{ left: x, top: y }}
-          style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
-          onMouseDown={(e) => {
-            const initX = e.screenX
-            const initY = e.screenY
-
-            const mouseMoveHandler = (e: MouseEvent) => {
-              console.log('mouseMovie')
-              setPosition({
-                x: x + e.screenX - initX,
-                y: y + e.screenY - initY,
-              })
-            }
-            const mouseDownHandler = (e: MouseEvent) => {
-              console.log('mouseDown')
-              // document.removeEventListener('mousemove', mouseMoveHandler)
-            }
-            const mouseUpHandler = (e: MouseEvent) => {
-              console.log('mouseUp')
-              document.removeEventListener('mousemove', mouseMoveHandler)
-            }
-
-            document.addEventListener('mousemove', mouseMoveHandler)
-            document.addEventListener('mouseup', mouseUpHandler, { once: true })
-            document.addEventListener('mousedown', mouseDownHandler, {
-              once: true,
-            })
-          }}
-        >
-          <img
-            src="https://i.ibb.co/jhCmmyx/F8-VNCi-Ja-QAAge-Mg.jpg"
-            alt="sdads"
-            draggable={'false'}
-          />
-          <div />
-        </div>
-      </div>
-    </div>
+    <>
+      <span>
+        Drag : (x:{x} y:{y})
+      </span>
+      <DragComponent
+        x={x}
+        y={y}
+        onMouseDown={(e) => mouseEventHander(e, setPosition, x, y)}
+      >
+        <Image
+          src="https://i.ibb.co/jhCmmyx/F8-VNCi-Ja-QAAge-Mg.jpg"
+          alt="sdads"
+          draggable={'false'}
+          width={300}
+          height={300}
+        />
+      </DragComponent>
+    </>
   )
 }
+
+const DragComponent = styled.div<{ x: number; y: number }>`
+  transform: ${({ x, y }) => `translateX(${x}px) translateY(${y}px)`};
+`
