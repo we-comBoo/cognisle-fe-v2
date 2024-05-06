4f8caffa1f1ff3897e43913524036dc06fa2adc4
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-05-07 02:30:49 +0900

    feat: make image draggable according to X,Y without ghost Image

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: false,
   skipMiddlewareUrlNormalize: true,
+  images: {
+    domains: ['i.ibb.co', 'localhost'],
+  },
 }
 
 export default nextConfig
--- src/pages/drag/index.tsx
+++ src/pages/drag/index.tsx
@@ -1,5 +1,61 @@
-const Drag = () => {
-  return <div>Drag</div>
-}
+import React, { useState } from 'react'
 
-export default Drag
+export default function DragExample() {
+  const [{ x, y }, setPosition] = useState({
+    x: 0,
+    y: 0,
+  })
+
+  return (
+    <div className="p-4">
+      <div className="mb-2">
+        <h1 className="text-3xl font-bold">Drag</h1>
+        <span>without boundary bounce</span>
+        <span className="ml-4">
+          x:{x} y:{y}
+        </span>
+      </div>
+
+      <div className="grid items-center justify-center">
+        <div
+          className="h-24 w-24"
+          // style={{ left: x, top: y }}
+          style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
+          onMouseDown={(e) => {
+            const initX = e.screenX
+            const initY = e.screenY
+
+            const mouseMoveHandler = (e: MouseEvent) => {
+              console.log('mouseMovie')
+              setPosition({
+                x: x + e.screenX - initX,
+                y: y + e.screenY - initY,
+              })
+            }
+            const mouseDownHandler = (e: MouseEvent) => {
+              console.log('mouseDown')
+              // document.removeEventListener('mousemove', mouseMoveHandler)
+            }
+            const mouseUpHandler = (e: MouseEvent) => {
+              console.log('mouseUp')
+              document.removeEventListener('mousemove', mouseMoveHandler)
+            }
+
+            document.addEventListener('mousemove', mouseMoveHandler)
+            document.addEventListener('mouseup', mouseUpHandler, { once: true })
+            document.addEventListener('mousedown', mouseDownHandler, {
+              once: true,
+            })
+          }}
+        >
+          <img
+            src="https://i.ibb.co/jhCmmyx/F8-VNCi-Ja-QAAge-Mg.jpg"
+            alt="sdads"
+            draggable={'false'}
+          />
+          <div />
+        </div>
+      </div>
+    </div>
+  )
+}
