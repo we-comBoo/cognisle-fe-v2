f396278192a007c8cf6dc29ff009fa42e8eea700
JangAyeon <ayeon3052@ewhain.net>
2024-05-26 04:29:22 +0900

    #22 feat: add checkbox for remember email input value

--- src/components/pages/Login/form/index.tsx
+++ src/components/pages/Login/form/index.tsx
@@ -1,6 +1,6 @@
 import useLoginForm from '@/components/pages/Login/form/hook'
 import { useRouter } from 'next/router'
-
+import { useRef } from 'react'
 const LoginForm = () => {
   const router = useRouter()
   const goSignupPage = () => {
@@ -8,6 +8,18 @@ const LoginForm = () => {
   }
 
   const { inputRefs, errorMsg, submitLoginForm } = useLoginForm()
+  const checkBoxRef = useRef<HTMLInputElement>(null)
+
+  const handleEmailFlagCheck = () => {
+    const emailFlagCheck = checkBoxRef.current.checked
+    // console.log(emailFlagCheck, inputRefs.current[0].value)
+    if (emailFlagCheck) {
+      localStorage.setItem('LS_EMAIL', inputRefs.current[0].value)
+    } else {
+      localStorage.removeItem('LS_EMAIL')
+    }
+  }
+
   return (
     <section onSubmit={submitLoginForm}>
       <form>
@@ -18,6 +30,7 @@ const LoginForm = () => {
           ref={(el) => {
             if (el !== null) inputRefs.current[0] = el
           }}
+          defaultValue={localStorage.getItem('LS_EMAIL')}
         />
         {errorMsg.email && <p>{errorMsg.email}</p>}
         <label htmlFor="password">비밀번호</label>
@@ -29,6 +42,16 @@ const LoginForm = () => {
           }}
         />
         {errorMsg.password && <p>{errorMsg.password}</p>}
+        <div>
+          <input
+            type="checkbox"
+            id="rememberEmail"
+            ref={checkBoxRef}
+            defaultChecked={false}
+            onChange={handleEmailFlagCheck}
+          />
+          <label htmlFor="rememberId">아이디 기억하기</label>
+        </div>
         <button type="submit">로그인 하기</button>
       </form>
       <div>
