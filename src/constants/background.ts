8a54b623d6eebd203be90f1957fb33c70d011c45
JangAyeon <ayeon3052@ewhain.net>
2024-05-26 01:30:47 +0900

    feat: add background assets info for login page

---
+++ src/constants/background.ts
@@ -0,0 +1,12 @@
+const background = {
+  login: {
+    img: { src: '/assets/background/triangle.png', width: 43, height: 84.9 },
+    color: {
+      start: '--gradient-yellow',
+      end: '--color-green-03',
+      degree: '180deg',
+    },
+  },
+}
+
+export default background
