0d85ec42c2be50ae82747df8731353db90903574
JangAyeon <ayeon3052@ewhain.net>
2024-05-27 18:33:59 +0900

    style: make input checkbox style for common component

---
+++ src/components/common/index.ts
@@ -0,0 +1,4 @@
+import { Button, FormBtn } from './Button'
+import { CheckBox, CheckBoxWrapper } from './CheckBox'
+
+export { Button, FormBtn, CheckBox, CheckBoxWrapper }
--- src/components/pages/Login/form/style.ts
+++ src/components/pages/Login/form/style.ts
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled'
 import { FONTS } from '@/styles/font'
-import { FormBtn } from '@/components/common/Button'
+import { FormBtn, CheckBox, CheckBoxWrapper } from '@/components/common'
 
 export const St = {
   Section: styled.section`
@@ -16,29 +16,14 @@ export const St = {
     gap: 2.4rem;
     margin-bottom: 4.8rem;
   `,
-  CheckBoxWrapper: styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
+  CheckBoxWrapper: styled(CheckBoxWrapper)`
     margin-bottom: 2.4rem;
     label {
       margin-left: 0.8rem;
       ${FONTS.caption1}
     }
   `,
-  CheckBox: styled.input<{ checked: boolean; imgSrc: string }>`
-    border: solid 0.3rem var(--color-green-400);
-    width: 2.2rem;
-    height: 2.2rem;
-    background-color: ${({ checked }) =>
-      checked ? `var(--color-green-400)` : 'transparent'};
-    background-image: ${({ checked, imgSrc }) =>
-      checked ? `url(${imgSrc})` : 'none'};
-
-    background-size: 1.5rem 1.2rem;
-    background-repeat: no-repeat;
-    background-position: center;
-  `,
+  CheckBox: styled(CheckBox)``,
 
   Input: styled.input`
     background-color: var(--color-yellow-100);
