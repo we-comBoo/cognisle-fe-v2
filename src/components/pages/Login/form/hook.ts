d8e303e632633694f0a726e14cde157cfc474ef3
JangAyeon <ayeon3052@ewhain.net>
2024-05-28 16:10:34 +0900

    fix: FormEvent import 추가:

--- src/components/pages/Login/form/hook.ts
+++ src/components/pages/Login/form/hook.ts
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { FormEvent, useRef, useState } from 'react'
 import { signIn } from 'next-auth/react'
 import useRouter from 'next/router'
 const useLoginForm = () => {
