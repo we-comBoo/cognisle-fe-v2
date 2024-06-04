83b97c92646706fdcaa3a7696e6b837bb377192d
JangAyeon <ayeon3052@ewhain.net>
2024-06-05 02:19:02 +0900

    #29 feat: 하단 메뉴 컴포넌트 추가

---
+++ src/components/common/Divider/index.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+
+const Divider = () => {
+  return <DividerWrapper></DividerWrapper>
+}
+
+export default Divider
+const DividerWrapper = styled.div`
+  height: 3.5rem;
+  width: 0.1rem;
+  background-color: var(--color-green-200);
+`
---
+++ src/types/bottomMenu.ts
@@ -0,0 +1,10 @@
+export interface BottomMenuInfoProps {
+  link: string
+  image: {
+    active: string
+    inactive: string
+    width: number
+    height: number
+    alt: string
+  }
+}
