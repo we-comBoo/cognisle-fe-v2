88a283285e7534c139eb60a33e08a6fe27098d80
JangAyeon <ayeon3052@ewhain.net>
2024-06-05 02:19:50 +0900

    #29 chore: 이미지 png to svg 교체

--- src/constants/UI/logo.ts
+++ src/constants/UI/logo.ts
@@ -2,13 +2,13 @@ import { logoTypeInfoProps } from '@/types'
 
 export const LOGO_TYPE_INFO: logoTypeInfoProps = {
   primary: {
-    src: '/assets/logo/primary.png',
+    src: '/assets/logo/primary.svg',
     width: 240,
     height: 124,
     alt: 'primary/vertical logo',
   },
   secondary: {
-    src: '/assets/logo/secondary.png',
+    src: '/assets/logo/secondary.svg',
     width: 185,
     height: 32,
     alt: 'secondary/horizontal logo',
