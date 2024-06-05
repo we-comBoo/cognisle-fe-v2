e4e01501d88a1f4a24a6592f555ab11c50f55570
JangAyeon <ayeon3052@ewhain.net>
2024-06-05 14:16:47 +0900

    #32 feat: 모달 밖의 영역 클릭시 제어 훅 추가

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,32 @@
+import { RefObject, useEffect } from 'react'
+const useOutsideClick = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: Event) => void,
+) => {
+  useEffect(() => {
+    const listener = (event: Event) => {
+      console.log('      ㅍㅎ퍼ㅗ', ref.current)
+      console.log('ㅗ퍼ㅗ', event.target)
+      if (!ref.current) {
+        return
+      } else if (
+        ref.current &&
+        event.target &&
+        !ref.current.contains(event.target as Node)
+      ) {
+        handler(event)
+      }
+    }
+
+    document.addEventListener('click', listener)
+    // document.addEventListener("mousedown", listener);
+    // document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener('click', listener)
+      // document.removeEventListener("mousedown", listener);
+      // document.removeEventListener("touchstart", listener);
+    }
+  }, [ref, handler]) // Reload only if ref or handler changes
+}
+
+export default useOutsideClick
