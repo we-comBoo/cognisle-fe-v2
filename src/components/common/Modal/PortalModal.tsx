e5163bde80a3ad604ac1376a56e9d5e2f8269da3
JangAyeon <ayeon3052@ewhain.net>
2024-06-05 14:15:49 +0900

    #32 feat: react Portal 이용한 모달 컴포넌트 추가

---
+++ src/components/common/Modal/PortalModal.tsx
@@ -0,0 +1,23 @@
+import { ReactElement, useEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
+
+const Portal = ({ children }: { children: ReactElement }) => {
+  const [mounted, setMounted] = useState<boolean>(false)
+
+  useEffect(() => {
+    setMounted(true)
+    return () => setMounted(false)
+  }, [])
+
+  {
+    /*if (typeof window === 'undefined') return <></>*/
+  }
+
+  return mounted ? (
+    createPortal(children, document.getElementById('modal-root') as HTMLElement)
+  ) : (
+    <></>
+  )
+}
+
+export default Portal
