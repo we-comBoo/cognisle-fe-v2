c7b2cde2de9c41cdc8e924ea37be4a6da8419a76
JangAyeon <ayeon3052@ewhain.net>
2024-06-18 02:18:17 +0900

    #44 feat: 회원가입 입력 정보 상수 데이터 추가

---
+++ src/constants/form/signup.ts
@@ -0,0 +1,30 @@
+export const SIGNUP_FORM = [
+  {
+    label: '이메일',
+    id: 'email',
+    type: 'text',
+    placeholder: '이메일',
+    name: 'email',
+  },
+  {
+    label: '비밀번호',
+    id: 'password',
+    type: 'password',
+    placeholder: '비밀번호',
+    name: 'password',
+  },
+  {
+    label: '이름',
+    id: 'name',
+    type: 'text',
+    placeholder: '이름',
+    name: 'name',
+  },
+  {
+    label: '디스코드 아이디',
+    id: 'discordId',
+    type: 'text',
+    placeholder: '디스코드 아이디',
+    name: 'discordId',
+  },
+]
