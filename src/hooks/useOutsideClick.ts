40fe01a62947506617fcde77dc25bdaacdb13ce9
JangAyeon <ayeon3052@ewhain.net>
2024-06-18 11:17:11 +0900

    #44 feat: 선택 영역 클릭 감지 필요한 경우만 탐지하게 변경

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -1,15 +1,16 @@
 import { RefObject, useEffect } from 'react'
 const useOutsideClick = <T extends HTMLElement = HTMLElement>(
-  ref: RefObject<T>,
+  ref: RefObject<T> | null,
   handler: (event: Event) => void,
 ) => {
   useEffect(() => {
     const listener = (event: Event) => {
-      console.log('      ㅍㅎ퍼ㅗ', ref.current)
+      console.log('      ㅍㅎ퍼ㅗ', ref?.current)
       console.log('ㅗ퍼ㅗ', event.target)
-      if (!ref.current) {
+      if (ref && !ref.current) {
         return
       } else if (
+        ref &&
         ref.current &&
         event.target &&
         !ref.current.contains(event.target as Node)
