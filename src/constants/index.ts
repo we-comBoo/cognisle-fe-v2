f6da55cecc2c526d759463a72720a16caeb015b6
JangAyeon <ayeon3052@ewhain.net>
2024-06-18 12:42:35 +0900

    #44 feat: 입력값 검증 로직 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,7 +7,13 @@ import { withAuthList, withOutAuthList, FALLBACK_URL } from './routeUrl'
 import { BOTTOM_MENU_INFO } from './menu/bottom'
 import { STATE_MODAL_TYPE_INFO } from './modal/state'
 import { USER_POINT_BTN, MENU_POINT_BTN } from './common/pointBtn'
-import { SIGNUP_FORM, SIGNUP_INITIAL_VALUES, signupProps } from './form/signup'
+import {
+  SIGNUP_FORM,
+  SIGNUP_INITIAL_VALUES,
+  SIGNUP_VALIDATION,
+  signupProps,
+  signupValidationProps,
+} from './form/signup'
 export {
   BACKGROUND_TYPE_INFO,
   IMAGE_ADDRESS,
@@ -23,5 +29,6 @@ export {
   MENU_POINT_BTN,
   SIGNUP_FORM,
   SIGNUP_INITIAL_VALUES,
+  SIGNUP_VALIDATION,
 }
-export type { signupProps }
+export type { signupProps, signupValidationProps }
