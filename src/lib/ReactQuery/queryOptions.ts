1066353f759768252c5d7d19d0abf0e3b56bc503
JangAyeon <ayeon3052@ewhain.net>
2024-08-11 18:44:19 +0900

    #68: queryFn 인자 없는 경우 실행 X

--- src/lib/ReactQuery/queryOptions.ts
+++ src/lib/ReactQuery/queryOptions.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { User } from 'next-auth'
 
 const defaultKey = {
   island: ['island'] as const,
@@ -19,15 +20,20 @@ const queryKeys = {
   }) => [...queryKeys.detailComments(photoId), commentId] as const,
    */
 }
-
+// OK
+/*
+async () => {
+  return await api.getProfileByUsername({ username });
+}*/
 export const queryOptions = {
-  island: (ownerId: number) => ({
+  island: (ownerId: User['user_id']) => ({
     queryKey: queryKeys.island(ownerId),
 
     queryFn: async () => {
       const response = await axios.get(`/api/lands/${ownerId}`)
       return response.data
     },
+    enabled: !!ownerId,
   }),
   /*
   comments: (photoId: number) => ({
