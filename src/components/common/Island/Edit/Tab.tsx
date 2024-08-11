5a956a8ec0fd052c6139c9118794950b516e14aa
JangAyeon <ayeon3052@ewhain.net>
2024-08-11 18:47:56 +0900

    #68: 꾸미기 상자 탭 UI

---
+++ src/components/common/Island/Edit/Tab.tsx
@@ -0,0 +1,38 @@
+import { TabMenuProps } from '@/types/island/edit'
+import { Dispatch, SetStateAction } from 'react'
+import { PointBtn } from '@/components/common'
+import { EDIT_TAB_POINT_BTN } from '@/constants'
+import styled from '@emotion/styled'
+import { FONTS } from '@/styles/font'
+interface TabProps {
+  menu: TabMenuProps[]
+  tabId: TabMenuProps['id']
+  setId: Dispatch<SetStateAction<number>>
+}
+
+const Tab = ({ menu, tabId, setId }: TabProps) => {
+  const handleTabClick = (id: TabMenuProps['id']) => {
+    setId(id)
+  }
+  return (
+    <Wrapper>
+      {menu.map(({ id, title, value }, idx) => (
+        <PointBtn item={EDIT_TAB_POINT_BTN} key={id}>
+          <Text onClick={() => handleTabClick(id)}>{title}</Text>
+        </PointBtn>
+      ))}
+    </Wrapper>
+  )
+}
+
+export default Tab
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const Text = styled.span`
+  ${FONTS.body5}
+  color:var(--color-green-400)
+`
--- src/constants/styles/pointBtn.ts
+++ src/constants/styles/pointBtn.ts
@@ -12,6 +12,21 @@ const MENU_POINT_BTN: PointBtnInfoProps = {
   },
   round: true,
 }
+const EDIT_TAB_POINT_BTN: PointBtnInfoProps = {
+  size: {
+    width: 12,
+    height: 4,
+  },
+  point: {
+    width: 30,
+    height: 85,
+  },
+  color: {
+    main: '--color-yellow-100',
+    point: '--color-green-400',
+  },
+  round: false,
+}
 
 const USER_POINT_BTN: PointBtnInfoProps = {
   size: {
@@ -44,4 +59,9 @@ const GAME_START_POINT_BTN: PointBtnInfoProps = {
   },
   round: true,
 }
-export { MENU_POINT_BTN, USER_POINT_BTN, GAME_START_POINT_BTN }
+export {
+  EDIT_TAB_POINT_BTN,
+  MENU_POINT_BTN,
+  USER_POINT_BTN,
+  GAME_START_POINT_BTN,
+}
