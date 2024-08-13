46b1a976eb7a113374d369e3e5bba71de4835057
JangAyeon <ayeon3052@ewhain.net>
2024-08-13 19:15:49 +0900

    chore: 미들웨어 정규 표현식에 svg 미포함 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -78,5 +78,7 @@ export default async function middleware(req: NextRequest) {
 
 // 미들웨어가 실행될 특정 pathname을 지정하면, 해당 pathname에서만 실행 가능
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.svg$).*)',
+  ],
 }
