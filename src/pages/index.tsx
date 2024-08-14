7ddcbe97e2f088a9b2f32d4c2cc8e87c11b438fb
JangAyeon <ayeon3052@ewhain.net>
2024-08-14 16:53:23 +0900

    fix: 홈화면에서 로그인 유저에 대한 라우팅 클라이언트 사이드 조정 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,16 @@ import Logo from '@/components/common/Logo'
 import Triangle from '@/components/common/Triangle'
 import { useTimer } from '@/hooks'
 import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
 
 export default function Home() {
   // 2초 뒤에 홈 메뉴 화면으로 이동
   const router = useRouter()
+  const { data: session } = useSession()
 
-  useTimer(2000, () => router.push('/home'))
+  useTimer(2000, () =>
+    session?.user.access ? router.push('/home') : router.push('/login'),
+  )
 
   return (
     <>
