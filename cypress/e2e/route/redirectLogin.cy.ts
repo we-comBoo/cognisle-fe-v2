d76c53c54d72abbe613fd72b5c2ed597b92d6696
JangAyeon <ayeon3052@ewhain.net>
2024-08-15 18:22:18 +0900

    refactor: 보호 페이지 중 랜덤 선택해 진행하는 방식으로 변경

--- cypress/e2e/route/redirectLogin.cy.ts
+++ cypress/e2e/route/redirectLogin.cy.ts
@@ -1,9 +1,12 @@
-describe('Login and Redirect Flow', () => {
-  const loginUrl = '/login' // 로그인 페이지 URL
-  const homeUrl = '/home' // 로그인 후 접근할 페이지 URL
-  const email = 'test@gmail.com' // 테스트에 사용할 사용자 이름
-  const password = 'testDEV1234!' // 테스트에 사용할 비밀번호
+import {
+  WITH_AUTH_URLs,
+  loginUrl,
+  email,
+  password,
+  getRandomInt,
+} from './utils'
 
+describe('Redirect Login after Logout', () => {
   beforeEach(() => {
     // 테스트 전에 기본 URL로 돌아가도록 설정
     cy.visit('/')
@@ -11,7 +14,9 @@ describe('Login and Redirect Flow', () => {
 
   it('should redirect to login page if not logged in and trying to access a protected page', () => {
     // 대시보드 페이지 접근 시 로그인 페이지로 리다이렉트되는지 확인
-    cy.visit(homeUrl)
+    const protectedURL =
+      WITH_AUTH_URLs[getRandomInt(0, WITH_AUTH_URLs.length - 1)]
+    cy.visit(protectedURL)
     cy.url().should('include', loginUrl)
     // 로그인 폼에 데이터 입력
     // 인풋 요소를 찾고 텍스트를 입력합니다.
@@ -20,10 +25,18 @@ describe('Login and Redirect Flow', () => {
     cy.get('input[name="password"]')
       .type(password)
       .should('have.value', password)
+    cy.intercept('POST', '/api/auth/callback/credentials').as('login')
     // 로그인 버튼 클릭
     cy.get('button[type="submit"]').click()
+    cy.wait('@login')
 
     // 로그인 성공 후 대시보드 페이지로 리다이렉트되는지 확인
-    cy.url().should('eq', Cypress.config().baseUrl + homeUrl)
+    cy.url().should('eq', Cypress.config().baseUrl + protectedURL)
+    cy.visit('/home')
+    // 'aria-label' 속성을 사용하여 '로그아웃' 버튼 찾기
+    cy.get('button[aria-label="logout"]') // 로그아웃 버틎
+      .contains('로그아웃')
+      .click()
+    cy.url().should('include', loginUrl)
   })
 })
