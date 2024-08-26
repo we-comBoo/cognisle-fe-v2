0c4626368605759408da8ae884ccfedc037305df
JangAyeon <ayeon3052@ewhain.net>
2024-08-26 12:16:24 +0900

    #104 feat: 섬상태 수정 업데이트 API 연결과 리액트 쿼리 mutation 연결

--- src/components/common/Island/Edit/ItemSelect.tsx
+++ src/components/common/Island/Edit/ItemSelect.tsx
@@ -4,33 +4,48 @@ import styled from '@emotion/styled'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { useSwiper } from '@/hooks'
-import { useItemsStore } from '@/store/island/items'
-import { ItemProps } from '@/types/island'
+import { useItemsActions, useItemsStore } from '@/store/island/items'
+import { ItemInfoProps } from '@/types/island'
 
 const ItemSelect = () => {
   const items = useItemsStore()
+  const { removeItem, addItem } = useItemsActions()
   const { swiperSetting, currentSlide } = useSwiper()
   const handleItemClick = (
-    no: ItemProps['no'],
-    show: ItemProps['locations']['show'],
+    no: ItemInfoProps['no'],
+    show: ItemInfoProps['locations']['show'],
   ) => {
-    console.log(no, show)
+    if (no) {
+      if (show) {
+        removeItem(no) // remove
+      } else {
+        addItem(no)
+      }
+    }
   }
   return (
     <SliderWrapper>
       <Swiper {...swiperSetting}>
-        {items.map(({ no, item_image, locations: { show } }, idx) => (
-          <SwiperSlide key={idx}>
-            <Image
-              src={item_image}
-              alt={`item_${no}`}
-              draggable={'false'}
-              width={100}
-              height={100}
-              onClick={() => handleItemClick(no, show)}
-            />
-          </SwiperSlide>
-        ))}
+        {Object.entries(items).map(
+          ([
+            no,
+            {
+              item_image,
+              locations: { x, y, z, show },
+            },
+          ]) => (
+            <SwiperSlide key={no}>
+              <Image
+                src={item_image}
+                alt={`item_${no}`}
+                draggable={'false'}
+                width={100}
+                height={100}
+                onClick={() => handleItemClick(Number(no), show)}
+              />
+            </SwiperSlide>
+          ),
+        )}
       </Swiper>
     </SliderWrapper>
   )
--- src/pages/api/lands/item.ts
+++ src/pages/api/lands/item.ts
@@ -1,24 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { ItemProps } from '@/types/island'
 import { createAuthAxios } from '../axios'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { items, land } = req.body
-  /*
-  const itemsArr = items.map(
-    ({ no, locations: { x, y, z, show } }: ItemProps) => ({
-      no,
-      x,
-      y,
-      z,
-      show,
-    }),
-  )
-  const data = { land_back_id: land.type, items: itemsArr }
+  const { items, land_back_id } = req.body
+  const data = { items, land_back_id }
 
   try {
     const authAxios = await createAuthAxios(req, res)
@@ -30,30 +19,7 @@ export default async function handler(
   } catch (error) {
     console.log(error)
     res.status(404).json({ data: error })
-  }*/
+  }
   //console.log('handler', req.body, req.url)
   res.status(200).json({ name: 'John Doe' })
 }
-
-/*
-
-
-{
-    "locations":[
-        {
-            "item_no":1,
-            "x":10,
-            "y":11,
-            "z":20
-        },
-        {
-            "item_no":20,
-            "x":20,
-            "y":11,
-            "z":3
-        }
-    ],"land_back_id":2
-
-}
-
-*/
