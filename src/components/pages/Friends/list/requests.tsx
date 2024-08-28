0bab670536cd4fb51d680b07d464251b54097a65
JangAyeon <ayeon3052@ewhain.net>
2024-08-29 05:02:49 +0900

    #109 feat: 나에게 온 친구 신청 조회

--- src/components/pages/Friends/list/requests.tsx
+++ src/components/pages/Friends/list/requests.tsx
@@ -1,28 +1,43 @@
 import { Container, Divider, PrimaryBtn, SecondaryBtn } from '../common/style'
 import styled from '@emotion/styled'
-
-const RequestItem = () => {
+import { useSession } from 'next-auth/react'
+import { queryOptions } from '@lib/ReactQuery/queryOptions'
+import { useQuery } from '@tanstack/react-query'
+import { FriendProps } from '@/types/friends'
+const RequestItem = ({ name, email }: FriendProps) => {
+  const handleAcceptBtn = () => {
+    console.log('handleAcceptBtn', email)
+  }
+  const handleDismissBtn = () => {
+    console.log('handleDismissBtn', email)
+  }
   return (
     <Container>
-      <div>list</div>
+      <div>{name}</div>
       <BtnContainer>
-        <PrimaryBtn>수락</PrimaryBtn>
-        <SecondaryBtn>거절</SecondaryBtn>
+        <PrimaryBtn onClick={handleAcceptBtn}>수락</PrimaryBtn>
+        <SecondaryBtn onClick={handleDismissBtn}>거절</SecondaryBtn>
       </BtnContainer>
     </Container>
   )
 }
 
 const Requests = () => {
-  const numbers = Array.from({ length: 100 }, (_, index) => index + 1)
+  const { data: session } = useSession()
+  const ownerEmail = session?.user.email
+  const { queryKey, queryFn, enabled } =
+    queryOptions.myFriendsRequest(ownerEmail)
+  const { data: Friends } = useQuery({ queryKey, queryFn, enabled })
+  console.log(Friends)
   return (
     <>
-      {numbers.map((item, idx) => (
-        <>
-          <RequestItem />
-          {idx != numbers.length - 1 && <Divider />}
-        </>
-      ))}
+      {Friends &&
+        Friends.map(({ name, email }, idx) => (
+          <>
+            <RequestItem name={name} email={email} key={email} />
+            {idx != Friends.length - 1 && <Divider />}
+          </>
+        ))}
     </>
   )
 }
---
+++ src/pages/api/friends/[email].ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { createAuthAxios } from '../axios'
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { email } = req.query
+
+  try {
+    const authAxios = await createAuthAxios(req, res)
+
+    const { data } = await authAxios.get(`/friends/?email=${email}`)
+    console.log('GET /friends/email/ 결과', data)
+
+    res.status(200).json({ ...data })
+  } catch (error) {
+    console.log(error)
+    res.status(404).json({ data: error })
+  }
+}
